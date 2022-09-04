0b406191760b998293e513e229ebb1503d1d004b
Modenov <vmodenov@gmail.com>
2022-09-04 17:04:49 +0300

    Add some new files)))

--- Most-Frequent-Symbol.py
+++ Most-Frequent-Symbol.py
@@ -3,3 +3,16 @@
 #  Project: StepikPythonProjects
 #  File: Most-Frequent-Symbol.py
 
+# На вход программе подается строка текста. Напишите программу, которая выводит на экран символ,
+# который появляется наиболее часто.
+
+s = input()
+count = 0
+total = 0
+
+for i in s:
+    if s.count(i) >= count:
+        count = s.count(i)
+        total = i
+
+print(total)
