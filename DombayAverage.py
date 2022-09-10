c438363fdc29d3aab607ab97ff263f5ada220828
Modenov <vmodenov@gmail.com>
2022-09-10 18:46:23 +0300

    Add some new files...

---
+++ DombayAverage.py
@@ -0,0 +1,47 @@
+#  Copyright (c) Vladimir Modenov.
+#  Created: 10.09.2022, 18:05
+#  Project: StepikPythonProjects
+#  File: DombayAverage.py
+
+# давстур https://www.davstour.com/dombai
+davstour1 = 4.4
+davstour2 = 3.7
+davstour3 = 5.0
+davstourAverage = (davstour1 + davstour2 + davstour3) / 3
+
+# https://clubao.ru/tur-na-dombay
+clubSpb1 = 4.3
+clubSpb2 = 3.8
+clubSpb3 = 3.0
+clubSpbAverage = (clubSpb1 + clubSpb2 + clubSpb3) / 3
+
+# странник https://go.tutu.ru/tury/trekking/tour_3460/
+strange1 = 4.5
+strange2 = 4.6
+strange3 = 3.0
+strangeAverage = (strange1 + strange2 + strange3) / 3
+
+# восход https://club-voshod.com/about/o_klube/
+rise1 = 5.0
+rise2 = 4.9
+rise3 = 5.0
+riseAverage = (rise1 + rise2 + rise3) / 3
+
+# магазин путешествий https://magput.ru/?viewprog=13217
+travelShop1 = 4.8
+travelShop2 = 3.3
+travelShop3 = 3.6
+travelShopAverage = (travelShop1 + travelShop2 + travelShop3) / 3
+
+# YouTravel.me https://youtravel.me/
+youTravel1 = 4.6
+youTravel2 = 3.6
+youTravel3 = 3.5
+youTravelAverage = (youTravel1 + youTravel2 + youTravel3) / 3
+
+print('Давстур:', davstourAverage)
+print('Клуб "Странник":', strangeAverage)
+print('Турклуб Восход:', riseAverage)
+print('Маг. путешествий:', travelShopAverage)
+print('YOUTRAVEL.ME:', youTravelAverage)
+print('Активный отдых, СПб:', clubSpbAverage)
